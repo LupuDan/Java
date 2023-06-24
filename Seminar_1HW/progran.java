ebad0cc35ecce6e6271fca6421570f5425dbe56a
Lupu Daniel <dlupu666@gmail.com>
2023-06-24 11:23:38 +0300

    Task_4

--- Seminar_1HW/progran.java
+++ Seminar_1HW/progran.java
@@ -64,46 +64,78 @@
 
 // Реализовать простой калькулятор
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class Calculator {
+// public class Calculator {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+
+//         System.out.print("Введите первое число: ");
+//         double num1 = scanner.nextDouble();
+
+//         System.out.print("Введите оператор (+, -, *, /): ");
+//         char operator = scanner.next().charAt(0);
+
+//         System.out.print("Введите второе число: ");
+//         double num2 = scanner.nextDouble();
+
+//         double result = 0.0;
+
+//         switch (operator) {
+//             case '+':
+//                 result = num1 + num2;
+//                 break;
+//             case '-':
+//                 result = num1 - num2;
+//                 break;
+//             case '*':
+//                 result = num1 * num2;
+//                 break;
+//             case '/':
+//                 if (num2 != 0) {
+//                     result = num1 / num2;
+//                 } else {
+//                     System.out.println("Ошибка: деление на ноль");
+//                     return;
+//                 }
+//                 break;
+//             default:
+//                 System.out.println("Ошибка: некорректный оператор");
+//                 return;
+//         }
+
+//         System.out.println("Результат: " + result);
+//     }
+// }
+
+// (дополнительное задание) Задано уравнение вида q + w = e, q, w, e >= 0.
+// Некоторые цифры могут быть заменены знаком вопроса, например, 2? + ?5 = 69.
+// Требуется восстановить выражение до верного равенства.
+// Предложить хотя бы одно решение или сообщить, что его нет.
+
+
+public class EquationSolver {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        solveEquation();
+    }
 
-        System.out.print("Введите первое число: ");
-        double num1 = scanner.nextDouble();
-
-        System.out.print("Введите оператор (+, -, *, /): ");
-        char operator = scanner.next().charAt(0);
-
-        System.out.print("Введите второе число: ");
-        double num2 = scanner.nextDouble();
-
-        double result = 0.0;
-
-        switch (operator) {
-            case '+':
-                result = num1 + num2;
-                break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                if (num2 != 0) {
-                    result = num1 / num2;
-                } else {
-                    System.out.println("Ошибка: деление на ноль");
-                    return;
+    private static void solveEquation() {
+        for (int q = 0; q <= 9; q++) {
+            for (int w = 0; w <= 9; w++) {
+                for (int e = 0; e <= 9; e++) {
+                    if (isValidSolution(q, w, e)) {
+                        System.out.println("Решение найдено:");
+                        System.out.println(q + "? + ?" + w + " = " + e);
+                        return;
+                    }
                 }
-                break;
-            default:
-                System.out.println("Ошибка: некорректный оператор");
-                return;
+            }
         }
+        System.out.println("Решение не найдено.");
+    }
 
-        System.out.println("Результат: " + result);
+    private static boolean isValidSolution(int q, int w, int e) {
+        // Проверка условия q + w = e
+        return (q + w == e);
     }
 }
