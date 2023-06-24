016f468d3ca90ab8812caf444130a00d3dd09071
Lupu Daniel <dlupu666@gmail.com>
2023-06-24 11:21:28 +0300

    Task_3

--- Seminar_1HW/progran.java
+++ Seminar_1HW/progran.java
@@ -37,26 +37,73 @@
 
 // Вывести все простые числа от 1 до 1000
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Простые числа от 1 до 1000:");
-        for (int i = 2; i <= 1000; i++) {
-            if (isPrime(i)) {
-                System.out.println(i);
-            }
-        }
-    }
+// public class Main {
+//     public static void main(String[] args) {
+//         System.out.println("Простые числа от 1 до 1000:");
+//         for (int i = 2; i <= 1000; i++) {
+//             if (isPrime(i)) {
+//                 System.out.println(i);
+//             }
+//         }
+//     }
 
-    // Проверка, является ли число простым
-    private static boolean isPrime(int number) {
-        if (number <= 1) {
-            return false;
+//     // Проверка, является ли число простым
+//     private static boolean isPrime(int number) {
+//         if (number <= 1) {
+//             return false;
+//         }
+//         for (int i = 2; i <= Math.sqrt(number); i++) {
+//             if (number % i == 0) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
+
+
+// Реализовать простой калькулятор
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите первое число: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Введите оператор (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        System.out.print("Введите второе число: ");
+        double num2 = scanner.nextDouble();
+
+        double result = 0.0;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Ошибка: деление на ноль");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Ошибка: некорректный оператор");
+                return;
         }
-        for (int i = 2; i <= Math.sqrt(number); i++) {
-            if (number % i == 0) {
-                return false;
-            }
-        }
-        return true;
+
+        System.out.println("Результат: " + result);
     }
 }
