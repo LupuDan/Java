4132be7dbdf024196e7f49af6e1a669b2b8c4abc
Lupu Daniel <dlupu666@gmail.com>
2023-06-24 11:19:08 +0300

    Task_1_Fix

--- Seminar_1HW/progran.java
+++ Seminar_1HW/progran.java
@@ -1,33 +1,36 @@
-import java.util.Scanner;
+// Вычислить n-ое треугольного число (сумма чисел от 1 до n),
+// n! (произведение чисел от 1 до n)
+// import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите значение n: ");
-        int n = scanner.nextInt();
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+//         System.out.print("Введите значение n: ");
+//         int n = scanner.nextInt();
 
-        int triangularNumber = calculateTriangularNumber(n);
-        int factorial = calculateFactorial(n);
+//         int triangularNumber = calculateTriangularNumber(n);
+//         int factorial = calculateFactorial(n);
 
-        System.out.println("n-ое треугольное число: " + triangularNumber);
-        System.out.println("n-ый факториал: " + factorial);
-    }
+//         System.out.println("n-ое треугольное число: " + triangularNumber);
+//         System.out.println("n-ый факториал: " + factorial);
+//     }
 
-    // Вычисление n-ого треугольного числа
-    private static int calculateTriangularNumber(int n) {
-        int sum = 0;
-        for (int i = 1; i <= n; i++) {
-            sum += i;
-        }
-        return sum;
-    }
+//     // Вычисление n-ого треугольного числа
+//     private static int calculateTriangularNumber(int n) {
+//         int sum = 0;
+//         for (int i = 1; i <= n; i++) {
+//             sum += i;
+//         }
+//         return sum;
+//     }
 
-    // Вычисление факториала числа n
-    private static int calculateFactorial(int n) {
-        int factorial = 1;
-        for (int i = 1; i <= n; i++) {
-            factorial *= i;
-        }
-        return factorial;
-    }
-}
+//     // Вычисление факториала числа n
+//     private static int calculateFactorial(int n) {
+//         int factorial = 1;
+//         for (int i = 1; i <= n; i++) {
+//             factorial *= i;
+//         }
+//         return factorial;
+//     }
+// }
+
