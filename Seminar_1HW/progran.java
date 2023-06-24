d8118ba1a7ce1df6fa9e03963e19ca451d222de9
Lupu Daniel <dlupu666@gmail.com>
2023-06-24 11:19:39 +0300

    Task_2

--- Seminar_1HW/progran.java
+++ Seminar_1HW/progran.java
@@ -34,3 +34,29 @@
 //     }
 // }
 
+
+// Вывести все простые числа от 1 до 1000
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Простые числа от 1 до 1000:");
+        for (int i = 2; i <= 1000; i++) {
+            if (isPrime(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    // Проверка, является ли число простым
+    private static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
